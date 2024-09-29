0c1263327552cfb2826e2db288857b2e5efe22e2
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-29 12:44:52 -0400

    updates home page

--- app02/src/components/Home.js
+++ app02/src/components/Home.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className="container">
+            <h1>Welcome to My Portfolio</h1>
+            <p>
+                Hi! I'm Gustavo Saavedra, a passionate front-end developer.
+            </p>
+            <h2>My Mission</h2>
+            <p>
+                My goal is to delivery the best user experience
+                with a focus on simplicity and elegance.
+            </p>
+            <div className="button-container">
+                <Link to="/about">
+                    <button className="button">About Me</button>
+                </Link>
+                <Link to="/projects">
+                    <button className="button">My Projects</button>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
