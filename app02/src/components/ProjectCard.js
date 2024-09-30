9959ad73527b1300a2fc35a2d59b91ba3a454b6e
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-29 20:15:31 -0400

    ProjectCard some css

--- app02/src/components/ProjectCard.js
+++ app02/src/components/ProjectCard.js
@@ -1,8 +1,8 @@
 const ProjectCard = ({pic, title, description, link}) => {
 return (
-    <div>
-        <img src={pic}/>
+    <div className="project-card">
         <h3>{title}</h3>
+        <img src={pic}/>
         <p>{description}</p>
         <a href={link}>Repository</a>
     </div>
--- app02/src/components/Projects.js
+++ app02/src/components/Projects.js
@@ -1,5 +1,16 @@
+import ProjectCard from "./ProjectCard";
+import bugSmasher from "../assets/p-bugSmasher.png"
+
 const Projects = () => {
-    return <h1>My Projects</h1>;
+    return (
+        <div className="">
+            <div className="projects">
+                <ProjectCard  pic={bugSmasher} title={"Bug Smasher"} description={"This is a bug smasher game using only javascript to simulate the bug and click on it to smash it."} link={"somelink"}/>
+                <ProjectCard  pic={bugSmasher} title={"Bug Smasher"} description={"Some description here"} link={"somelink"}/>
+                <ProjectCard  pic={bugSmasher} title={"Bug Smasher"} description={"Some description here"} link={"somelink"}/>
+            </div>
+        </div>
+    );
 };
 
 export default Projects;
