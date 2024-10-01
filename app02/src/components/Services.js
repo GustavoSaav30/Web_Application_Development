ef5d308e04078f75754af40ca1b0ead4a850c03e
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-09-30 22:28:48 -0400

    upgrading the service page and adding the services

--- app02/src/components/Services.js
+++ app02/src/components/Services.js
@@ -1,11 +1,11 @@
 import ServiceCard from "./ServiceCard";
-import pic from "../assets/p-bugSmasher.png"
 function Services() {
     return (
         <div className="service-page">
             <h1>My services</h1>
             <div className="services">
-                <ServiceCard pic={pic} description={"Web development services developing web sites and application"}/>
+                <ServiceCard pic={"https://cdnwebkul.webkul.com/wp-content/uploads/2020/09/mobile-app-development-1.jpg"} description={"Mobile application development. Do your business need an app? Contact me for a quotation."}/>
+                <ServiceCard pic={"https://elitecoaching.io/wp-content/uploads/2022/05/Front-End-development--1536x1024.webp"} description={"Front-end web development. Creation, updates and upgrades of web sites front-end."}/>
             </div>
         </div>
     );
