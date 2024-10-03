8c28827afe9fd148d19670537c961cefd392974c
GustavoSaav30 <gsaaved2@my.centennialcollege.ca>
2024-10-03 10:46:34 -0400

    adding images

--- app02/src/components/Services.js
+++ app02/src/components/Services.js
@@ -1,11 +1,13 @@
 import ServiceCard from "./ServiceCard";
+import frontEnd from "../assets/front-end.webp"
+import mobileApp from "../assets/mobile-app.jpg"
 function Services() {
     return (
         <div className="service-page">
             <h1>My services</h1>
             <div className="services">
-                <ServiceCard pic={"https://cdnwebkul.webkul.com/wp-content/uploads/2020/09/mobile-app-development-1.jpg"} description={"Mobile application development. Do your business need an app? Contact me for a quotation."}/>
-                <ServiceCard pic={"https://elitecoaching.io/wp-content/uploads/2022/05/Front-End-development--1536x1024.webp"} description={"Front-end web development. Creation, updates and upgrades of web sites front-end."}/>
+                <ServiceCard pic={mobileApp} description={"Mobile application development. Do your business need an app? Contact me for a quotation."}/>
+                <ServiceCard pic={frontEnd} description={"Front-end web development. Creation, updates and upgrades of web sites front-end."}/>
             </div>
         </div>
     );
